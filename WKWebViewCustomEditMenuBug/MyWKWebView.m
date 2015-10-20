c049ea98019b18a26edc42933049cc9cb9949623
Dave Wieringa <dwjm@userwise.com>
2015-10-20 11:55:43 -0400

    Implement canPerformAction to alter Edit Menu

--- WKWebViewCustomEditMenuBug/MyWKWebView.m
+++ WKWebViewCustomEditMenuBug/MyWKWebView.m
@@ -10,12 +10,20 @@
 
 @implementation MyWKWebView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+    // KNOWN WKWEBVIEW BUG: http://stackoverflow.com/questions/26046098/wkwebview-and-uimenucontroller
+
+    NSLog(@"CHECKING CAN PERFORM ACTION : %@", NSStringFromSelector(action));
+
+    if (action == @selector(delete:))
+    {
+        // adding Delete as test (works)
+        return YES;
+    }
+    
+    // trying to remove everything else (does NOT work for Copy, Define, Share...)
+    return NO;
 }
-*/
 
 @end
