4e270845ce6496e2097ef7cd459115ed51c82c8b
Dave Wieringa <dwjm@userwise.com>
2015-10-20 11:41:04 -0400

    Initial commit

---
+++ WKWebViewCustomEditMenuBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewCustomEditMenuBug
+//
+//  Created by David Wieringa on 10/20/15.
+//  Copyright © 2015 Userwise Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
